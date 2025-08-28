acd86502c3c88338b79a799955f3848d74f02421
Shiva69-prasad <shiva31694@gmail.com>
2025-08-28 23:23:26 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+  int main(){
+      printf("hello world");
+      return 0;
+  }
+
+#include<stdio.h>
+
+  int main(){
+      int a=30,b=80,sum;
+      sum=a+b;
+printf("the sum is:%d",sum);
+return 0;
+  }
